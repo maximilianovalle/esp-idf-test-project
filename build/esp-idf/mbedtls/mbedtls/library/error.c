5ce579a0301cf6d1416040e6a88c127ee025394c
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-06-29 16:36:52 -0500

    Test menuconfig

---
+++ build/esp-idf/mbedtls/mbedtls/library/error.c
@@ -0,0 +1,1 @@
+/home/twofacedshark/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
---
+++ build/esp-idf/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
@@ -0,0 +1,1 @@
+/home/twofacedshark/esp/esp-idf/components/mbedtls/mbedtls/library/ssl_debug_helpers_generated.c
